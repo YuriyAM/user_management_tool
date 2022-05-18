8e2ecde2ffd8bb8ed3d7c6094011671ea4bf4be6
YuriyAM <mesda4@users.noreply.github.com>
2022-05-18 22:03:13 +0300

    Update page model, user is disabled by default

--- models/page.py
+++ models/page.py
@@ -2,7 +2,6 @@ from abc import ABC, abstractmethod
 
 
 class Page(ABC):
-    @staticmethod
     @abstractmethod
     def show(param=None):
         pass
--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 class User:
-    def __init__(self, username, password='', privileged=False, enabled=True):
+    def __init__(self, username, password='', privileged=False, enabled=False):
         self.username = username
         self.password = password
         self.privileged = privileged
